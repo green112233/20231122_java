381439aa1ab76e38d7538bfb4895b4fb96dd2b50
길선종 <sunjong0930@kookmin.kr>
2023-11-22 10:34:09 +0900

    first

---
+++ src/ch04_01/IfTest01.java
@@ -0,0 +1,18 @@
+package ch04_01;
+
+public class IfTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a  = 100;
+		
+		if(a>500) {
+			System.out.println("조건식은 참입니다!!");
+		} else {
+			System.out.println("조건식은 거짓입니다!!");
+		}
+		
+		System.out.println("저는 if문이 끝난뒤 나온 문장입니다.");
+	}
+
+}
